04a252748b155c4ad1216a28b7cc9e45e54af58f
Jordyn Thoreson <jordyn.k.thoreson@gmail.com>
2025-08-30 15:35:42 -0400

    modified index.js to create greet function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function greet() {
+    let userName = prompt("What is your name?")
+    alert("Hello, " + userName + "!")
+    let age = prompt("How old are you?")
+    let parsedAge = parseInt(age)
+    let userConfirmed = confirm("Select 'OK' if you have had a birthday this year. Select 'Cancel' if you have not.")
+    if (userConfirmed === true) {
+        alert("You were born in " + (new Date().getFullYear() - parsedAge) + ".")
+    } 
+    else {
+        alert("You were born in " + (new Date().getFullYear() - (parsedAge + 1)) + ".")
+    }
+}
